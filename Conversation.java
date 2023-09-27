a63b1f0c0a03d9aef9088bf3f98954744facb76e
Rose <rpbriggs@smith.edu>
2023-09-26 21:56:36 -0400

    Added some sentence formatting and fixed bugs

--- Conversation.java
+++ Conversation.java
@@ -5,23 +5,22 @@ import java.util.Arrays;
 class Conversation {
     static String[] openingStatements = {"What would you like to talk about today?", "What is on your mind?", "What's up?"};
     static String[] setResponses = {"That's great", "Very cool", "Neato", "How interesting"}; // create a list of strings that will be used to respond to unknown statements
-    static String[][] mirrorWords = {{"I", "you"}, {"me", "you"}, {"you", "I"}, {"am", "are"}, {"my", "your"}, {"your", "my"}};
+    static String[] closingStatements = {"See you later", "Goodbye", "Hope you have a good day"}; // a list of strings to end the conversation with
+    static String[][] mirrorWords = {{"I", "you"}, {"me", "you"}, {"you", "I"}, {"am", "are"}, {"my", "your"}, {"your", "my"}, {"Im", "you're"}, {"youre", "I'm"}}; // a 2d array of strings the user might input and what to replace them with
     static Random random = new Random(); // create a module to use for generating random numbers
     static Scanner input = new Scanner(System.in); // create a module to use for getting user input
     static String transcript[] = {};
-
 
     // You will start the conversation here.
     public static void main(String[] arguments) {
         int totalTurns = getNumTurns(); // get the total number of turns the user wants to take
 
-        int openingStatementIndex = random.nextInt(openingStatements.length);
-
-        System.out.println(openingStatements[openingStatementIndex]);
-        addStringToTranscript(openingStatements[openingStatementIndex]);
+        int openingStatementIndex = random.nextInt(openingStatements.length); // choose a random opening
+        System.out.println(openingStatements[openingStatementIndex]); // print said opening
+        addStringToTranscript(openingStatements[openingStatementIndex]); // append said opening to the transcript
 
         String userInput; // create a variable to hold the user's input
-        String outputString;
+        String outputString; // create a variable to hold the output from the computer
         for (int i=0; i < totalTurns+1; i++){ // run the following code the number of times the user asked for
             userInput = input.nextLine(); // get user input
             addStringToTranscript(userInput); // add the user's input to the transcript
@@ -29,12 +28,24 @@ class Conversation {
             System.out.println(outputString); // print the output
             addStringToTranscript(outputString); // add the output to the transcript
         }
+
+        int closingStatementIndex = random.nextInt(closingStatements.length); // choose a random closing
+        System.out.println(closingStatements[closingStatementIndex]); // print said random closing
+        addStringToTranscript(closingStatements[closingStatementIndex]); // append said closing to the transcript
+
+
+        System.out.println("\n\nTranscript:");
+        for(int i=0; i < transcript.length; i++){ // iterate over every line in the transcript
+            System.out.println(transcript[i]); // print the current line of the transcript
+        }
     }
+
     /**
-     * Checks the user input for key words. If there are keywords found, mirror them, if not, return a canned response
+     * Checks the user input for key words. If there are keywords found, mirror them, if not, return a canned response. The returned response has some formatting done in the form of capitalizing the first letter, replacing periods with question marks, and adding a question mark to the end if there isn't one
      * @param input takes the user's input to be checked
      */
     public static String getOutput(String input){
+        input = input.replace("'", ""); // remove apostrophes from the input to make mirroring easier
         String[] splicedString = input.split(" ");
         boolean useSetResponse = true;
         String stringToReturn;
@@ -48,14 +59,18 @@ class Conversation {
                 }
             }
         }
-
-        if(useSetResponse == true){
-            int responseIndex = random.nextInt(setResponses.length);
-            stringToReturn = setResponses[responseIndex];
+        if(useSetResponse == true){ // if no substitutions were made
+            int responseIndex = random.nextInt(setResponses.length); // choose a random response
+            stringToReturn = setResponses[responseIndex]; // set the string that will be returned to that response
         }
         else{
-            stringToReturn = String.join(" ", splicedString);
-            // TODO: Make first letter uppercase
+            stringToReturn = String.join(" ", splicedString); // recombines all of the strings with spaces between each one
+            stringToReturn = stringToReturn.replace('.', '?'); // replaces all periods with question marks
+            stringToReturn = stringToReturn.replaceFirst(String.valueOf(stringToReturn.charAt(0)), String.valueOf(stringToReturn.charAt(0)).toUpperCase()); // capitalizes the first letter
+
+            if(stringToReturn.endsWith("?") == false){ // if there isn't a question mark on the end
+                stringToReturn = stringToReturn + "?"; // put a question mark on the end
+            }
         }
 
         return stringToReturn;
@@ -86,6 +101,10 @@ class Conversation {
         return intToReturn;
     }
 
+    /**
+     * Appends a string to the transcript that will be printed at the end of the program
+     * @param stringToAdd The string that will be added to the transcript
+     */
     private static void addStringToTranscript(String stringToAdd){
         transcript = Arrays.copyOf(transcript, transcript.length +1);
         transcript[transcript.length - 1] = stringToAdd;
--- reflection.md
+++ reflection.md
@@ -1,10 +1,14 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    I briefly chatted with the person I was sitting next to (I'm so sorry I can't remember you name D: ) before class on 9/26
 - Which session(s) of TA / office hours did you attend?
+    None
 - What are your initial impressions of Java? 
     Honestly, it just feels like C# with extra steps at the moment... I get the feeling I will like it once I get more used to it, but for now it just feels clunky 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    Even though Java is an object oriented language, you can use it as a functional language if you want. This program is technically made inside of an object, but at the end of the day it is almost entirely functional programming.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    Please make it way more clear that consulting with other students is a requirement for this assignment earlier or even at the same time as it's assigned (if it even is a requirement at all I'm still not quite sure)
 - Other notes?
-    It would be really cool to do this type of thing again later but using a json file to hold a bunch of mirror words instead of a 2d array
+    It would be really cool to do this type of thing again later but using a json file to hold a bunch of mirror words instead of a 2d array. This could be a neat intro to data parsing if we get into different formats later in this class (or in another class)
